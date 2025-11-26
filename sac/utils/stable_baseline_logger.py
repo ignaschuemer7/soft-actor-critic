c37776f6acaf47e2704d0b24bce0769445237987
Fausto Pettinari <fpettinari@udesa.edu.ar>
2025-11-25 22:33:41 -0300

    logger for stabe_baselines added and used in ROBRE notebook

---
+++ sac/utils/stable_baseline_logger.py
@@ -0,0 +1,24 @@
+from stable_baselines3.common.callbacks import BaseCallback
+from torch.utils.tensorboard import SummaryWriter
+
+class RewardLoggingCallback(BaseCallback):
+    def __init__(self, writer: SummaryWriter, verbose=0):
+        super().__init__(verbose)
+        self.writer = writer
+        self.episode_reward = 0.0
+
+    def _on_step(self) -> bool:
+        # SB3 almacena la info del environment en "infos"
+        for info in self.locals.get("infos", []):
+            if "episode" in info:    # Gymnasium envs guardan info de episodios aquí
+                ep_reward = info["episode"]["r"]
+                ep_length = info["episode"]["l"]
+
+                # Log a TensorBoard
+                self.writer.add_scalar("Episode/Reward", ep_reward, self.num_timesteps)
+                self.writer.add_scalar("Episode/Length", ep_length, self.num_timesteps)
+
+                if self.verbose:
+                    print(f"Step={self.num_timesteps}, Reward={ep_reward}")
+
+        return True
